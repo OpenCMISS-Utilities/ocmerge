e0ffd390d7e491bf4f3ceb9360975bbabb39ed27
Gene Soudlenkov <g.soudlenkov@.auckland.ac.nz>
2020-05-29 14:16:53 +1200

    Added comments

--- src/ocmerge.cpp
+++ src/ocmerge.cpp
@@ -1,3 +1,47 @@
+/* \file
+ * \author Gene Soudlenkov
+ * \brief
+ *
+ * \section LICENSE
+ *
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is OpenCMISS
+ *
+ * The Initial Developer of the Original Code is University of Auckland,
+ * Auckland, New Zealand, the University of Oxford, Oxford, United
+ * Kingdom and King's College, London, United Kingdom. Portions created
+ * by the University of Auckland, the University of Oxford and King's
+ * College, London are Copyright (C) 2007-2010 by the University of
+ * Auckland, the University of Oxford and King's College, London.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ */
+
 #include <stdio.h>
 #include <string>
 #include <vector>
@@ -6,13 +50,14 @@
 
 using namespace std;
 
-
+//Node information
 struct Node
 {
 	int id;
 	std::vector<double> values;
 };
 
+//Element information
 struct Elem
 {
 	std::vector<int> id;
@@ -25,17 +70,21 @@ typedef std::vector<std::string> STRLIST;
 typedef std::vector<Node> NODELIST;
 typedef std::vector<Elem> ELEMLIST;
 typedef enum { UNDEF_TYPE=-1, ELEMENTS_TYPE=0, NODES_TYPE=1 } FILETYPE;
+//Words that identify information structures
 const char *stoppers[]={"Element","Node"};
 
-double precision=0.0000001;
-bool quiet=false;
-bool add_header=false;
+double precision=0.0000001; //Proximity precision
+bool quiet=false; //generate additional output for debugging
+bool add_header=false; //output headers
 
+
+//Returns true if two values are withing "precision" distance from each other
 inline bool close_enough(double v1,double v2)
 {
 	return fabs(v1-v2)<=precision;
 }
 
+//Returns true if two containers are equal
 template<class T>
 bool compare(const T& l1, const T& l2)
 {
@@ -47,6 +96,8 @@ bool compare(const T& l1, const T& l2)
 	return true;
 }
 
+//Returns true if two double-based containers are equal
+//using proximity comparison
 bool compare(const std::vector<double>& l1, const std::vector<double>& l2)
 {
 	if(l1.size()!=l2.size())
@@ -58,6 +109,7 @@ bool compare(const std::vector<double>& l1, const std::vector<double>& l2)
 }
 
 
+//expand file list if file name contains globing operations
 void expand_file(std::vector<std::string>& res,const std::string& pattern)
 {
 	glob_t glob_result;
@@ -78,6 +130,7 @@ void expand_file(std::vector<std::string>& res,const std::string& pattern)
 	globfree(&glob_result);
 }
 
+//expand file list if necessary
 void expand_file_list(std::vector<std::string>& lst)
 {
 	std::vector<std::string> res;
@@ -96,7 +149,7 @@ void expand_file_list(std::vector<std::string>& lst)
 	lst=res;
 }
 
-
+//Break a string into a series of substrings using delimiters
 void tokenize(const std::string& str,const std::string& delimiters,std::vector<std::string>& tokens)
 {
     // skip delimiters at beginning.
@@ -126,7 +179,7 @@ void tokenize(const std::string& str,const std::string& delimiters,std::vector<s
 }
 
 
-
+//Read an input file header until the stopper word found
 bool read_header(FILE *f,const std::string& stopper,std::string& str)
 {
 	bool res=false;
@@ -155,11 +208,14 @@ bool read_header(FILE *f,const std::string& stopper,std::string& str)
 	return res;
 }
 
+//Compare two nodes by their ids
 bool node_sort(Node& n1, Node& n2)
 {
 	return (n1.id < n2.id);
 }
 
+
+//Read node information
 void read_body_node(FILE *f,NODELIST& lst)
 {
 	bool res=false;
@@ -236,6 +292,8 @@ bool process_nodes(const std::string& hdr,NODELIST& src, const std::string& outp
 	return true;
 }
 
+
+//Sort elements using their IDs sequences
 bool elem_sort(Elem& n1, Elem& n2)
 {
 	if(n1.id.size()<n2.id.size())
